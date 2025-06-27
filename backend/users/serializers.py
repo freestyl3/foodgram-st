2cc2e7b794325e85d24fe31959cc397dc5118e46
Alexey Tyaguschev <lehatop450tav@gmail.com>
2025-06-27 22:49:27 +0300

    feat subscription on users

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,10 +2,21 @@ from rest_framework import serializers
 from djoser.serializers import UserSerializer
 
 from django.contrib.auth import get_user_model
+from .models import Subscription
 
 class FoodgramUserSerializer(UserSerializer):
     first_name = serializers.CharField(required=True)
     last_name = serializers.CharField(required=True)
+    # is_subscribed = serializers.SerializerMethodField()
+
+    def get_is_subscribed(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return Subscription.objects.filter(
+            user=obj,
+            follower=user
+        ).exists()
 
     class Meta(UserSerializer.Meta):
         model = get_user_model()
@@ -13,3 +24,9 @@ class FoodgramUserSerializer(UserSerializer):
             'email', 'id', 'username', 
             'first_name', 'last_name', 'avatar'
         )
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = ('__all__')
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,8 +1,12 @@
-from rest_framework import viewsets, permissions, serializers
+from rest_framework import viewsets, permissions, serializers, status
 from djoser.views import UserViewSet
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
 from django.contrib.auth import get_user_model
-from .serializers import FoodgramUserSerializer
+from .serializers import FoodgramUserSerializer, SubscriptionSerializer
+from .models import Subscription
 
 class FoodgramUserViewSet(UserViewSet):
     queryset = get_user_model().objects.all()
@@ -13,3 +17,40 @@ class FoodgramUserViewSet(UserViewSet):
             return (permissions.IsAuthenticatedOrReadOnly(), )
         return super().get_permissions()
     
+    @action(
+        methods=['post', 'delete'],
+        url_path='subscribe',
+        permission_classes=(permissions.IsAuthenticated, ),
+        detail=True
+    )
+    def subscribe(self, request, id):
+        follower = request.user
+        following_user = get_object_or_404(
+            get_user_model(),
+            id=id
+        )
+        if request.method == 'POST':
+
+            if (
+                follower == following_user or 
+                Subscription.objects.filter(
+                    user=following_user,
+                    follower=follower
+                ).exists()
+            ):
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+
+            subscription = Subscription.objects.create(
+                user=following_user,
+                follower=follower
+            )
+
+            serializer = SubscriptionSerializer(
+                subscription.user,
+                context={'request': request}
+            )
+
+            return Response(
+                data=serializer.data,
+                status=status.HTTP_201_CREATED
+            )
